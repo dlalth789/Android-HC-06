f5ac7fcfd60f6362a4f224348c893c3ba85e6433
paperrune <paperrune@naver.com>
2017-09-09 22:46:32 +0900

    Add files via upload

---
+++ Arduino/Arduino_Mega_2560.ino
@@ -0,0 +1,17 @@
+void setup(){
+  Serial.begin(9600);
+  Serial1.begin(9600);
+}
+
+void loop(){  
+  if(Serial.available()){    
+    while(Serial.available()){
+      Serial1.write(Serial.read());
+      delay(5);
+    }
+    Serial1.write('\0');
+  }
+  if(Serial1.available()){
+    Serial.write(Serial1.read());
+  }
+}
---
+++ Arduino/Arduino_Uno.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+ 
+SoftwareSerial BTSerial(2, 3);
+
+void setup(){
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+}
+
+void loop(){  
+  if(Serial.available()){
+    while(Serial.available()){
+      BTSerial.write(Serial.read());
+      delay(5);
+    }
+    BTSerial.write('\0');
+  }
+  if(BTSerial.available()){
+    Serial.write(BTSerial.read());
+  }
+}
